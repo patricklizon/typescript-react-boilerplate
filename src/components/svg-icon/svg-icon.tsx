2992374bf2762a147c571a25b9001fd8c5d8e1fc
patryklizon <lizon.patryk@gmail.com>
2019-03-07 00:02:05 +0100

    Renave SVGIcon component

--- src/components/svg-icon/svg-icon.tsx
+++ src/components/svg-icon/svg-icon.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 
 import { CommonHtmlProps } from "../../../typings/html";
 
-import * as styles from "./svg-inline.scss";
+import * as styles from "./svg-icon.scss";
 
 
 
@@ -16,7 +16,7 @@ interface ISVGIcon extends CommonHtmlProps {
 
 export const SVGIcon:React.SFC<ISVGIcon> = ({ svg, size }) => {
   function createMarkup (): React.ReactNode {
-    return <div className={cn(styles.icon, size)} dangerouslySetInnerHTML={{__html: svg}} />
+    return <div className={cn(styles.svgIcon, size)} dangerouslySetInnerHTML={{__html: svg}} />
   }
 
   return (<>{createMarkup()}</>)
